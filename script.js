04b1058d984ead8b696cb80f79c5d34537f67965
victoranjos-rgb <victor_anjos@estudante.sesisenai.org.br>
2026-03-23 12:55:34 -0300

    JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form-inspecao');
+const mensagem = document.getElementById('mensagem')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+});
