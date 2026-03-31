06c2a2a9718040662235bb1b83be4fa661b68071
victor_anjos@estudante.sesisenai.org.br <victor_anjos@estudante.sesisenai.org.br>
2026-03-31 08:42:12 -0300

    Terminando JS

--- script.js
+++ script.js
@@ -3,4 +3,45 @@ const mensagem = document.getElementById('mensagem')
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const setor = document.getElementById('setor').value;
+    const descricao = document.getElementById('descricao').value.trim();
+    const termos = document.getElementById('termos').checked;
+    const prioridade = document.querySelector('input[name="prioridade"]:checked');
+
+    if(nome.length >= 5)
+        console.log('nome valido')
+
+    if(nome === ''){
+        mensagem.textContent = 'Preencha o nome.';
+        return;
+    }
+
+    if(email ===''){
+        mensagem.textContent = 'Preencha o email.';
+        return;
+    }
+
+    if(setor === ''){
+        mensagem.textContent = 'Preencha o setor.';
+        return;
+    }
+
+    if(!prioridade){
+        mensagem.textContent = 'Selecione uma prioridade.';
+        return;
+    }
+
+    if(descricao.length < 10){
+        mensagem.textContent = 'A descrição deve ter pelo menos 10 caracteres';
+        return;
+    }
+
+    if(!termos){
+        mensagem.textContent = 'Marque a confirmação dos dados';
+        return;
+    }
+
+    mensagem.textContent = 'Formulário validado com sucesso.';
 });
